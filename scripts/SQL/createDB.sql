32d6215ae58fd1cae7335b9e95d37e34361dbefa
gyzzako <arnaud.berg1999@gmail.com>
2021-11-03 19:30:21 +0100

    supression ancien script

--- scripts/SQL/createDB.sql
+++ scripts/SQL/createDB.sql
@@ -41,33 +41,33 @@ create table meal(
     image bytea 
 );
 
-INSERT INTO "user"(id,firstname,lastname,phone_number,username,password,isAdmin,province,city,street_and_number) VALUES (1,"Arnaud","Berg","0498989898","arnaud_berg","1234",true,"Namur","Walonnie","Chatelet 96"),
-                                                                                                                        (2,"Donny Samuel","Mboma","0498989898","donny_dsm","4321",true,"Flandre Occidentale","Opwijk","kinstraat 26"),
-                                                                                                                        (3,"Emma","vdc","049888888","emma_manu","4444",false,"Namur","Namur","Namur 95"),
-                                                                                                                        (5,"Christopher","Dubois","04987898","dubois_chris","12345",false,"Bruxelles","Bruxelles Uccle","Royaume 25"),
-                                                                                                                        (6,"Cassy","Delambres","04987898","cassy_del","12345",false,"Bruxelles","Bruxelles Ixelles","Trone 44"),
-                                                                                                                        (7,"Richard","Fontaine","02223568","riri_fofo","9978",false,"Namur","Namur","ISEN 66"),
-                                                                                                                        (8,"Florent","Weiten","04987877","flo_weiten","4477",false,"Luxembourg","Luxembourg GD","Safir 25"),
-                                                                                                                        (9,"Louis","Hermant","04987777","louisiane_01","662345",false,"Namur","IESN","Henallux 25"),
-                                                                                                                        (10,"Boris","Clement","04987898","bobo_clems","72345",false,"Bruxelles","Bruxelles Ever","Prielstraat 12");
-INSERT INTO category(id,name) VALUES(1,"Burger"),
-                                     (2,"Pizza"),
-                                     (3,"Sushi"),
-                                     (4,"Plat Belge"),
-                                     (5,"Plat Espagnol");
+INSERT INTO "user"(firstname,lastname,phone_number,username,password,isAdmin,province,city,street_and_number) VALUES ('Arnaud','Berg','0498989898','arnaud_berg','1234',true,'Namur','Walonnie','Chatelet 96'),
+                                                                                                                        ('Donny Samuel','Mboma','0498989898','donny_dsm','4321',true,'Flandre Occidentale','Opwijk','kinstraat 26'),
+                                                                                                                        ('Emma','vdc','049888888','emma_manu','4444',false,'Namur','Namur','Namur 95'),
+                                                                                                                        ('Christopher','Dubois','04987898','dubois_chris','12345',false,'Bruxelles','Bruxelles Uccle','Royaume 25'),
+                                                                                                                        ('Cassy','Delambres','04987898','cassy_del','12345',false,'Bruxelles','Bruxelles Ixelles','Trone 44'),
+                                                                                                                        ('Richard','Fontaine','02223568','riri_fofo','9978',false,'Namur','Namur','ISEN 66'),
+                                                                                                                        ('Florent','Weiten','04987877','flo_weiten','4477',false,'Luxembourg','Luxembourg GD','Safir 25'),
+                                                                                                                        ('Louis','Hermant','04987777','louisiane_01','662345',false,'Namur','IESN','Henallux 25'),
+                                                                                                                        ('Boris','Clement','04987898','bobo_clems','72345',false,'Bruxelles','Bruxelles Ever','Prielstraat 12');
+INSERT INTO category(name) VALUES('Burger'),
+                                     ('Pizza'),
+                                     ('Sushi'),
+                                     ('Plat Belge'),
+                                     ('Plat Espagnol');
 
-INSERT INTO "order"(id,order_date,user_fk) VALUES (1,"2021-01-12",1),
-                                                  (2,"2021-07-07",3),
-                                                  (3,"2020-12-03",2),
-                                                  (4,"2022-06-14",5),
-                                                  (5,"2022-06-14",4),
-                                                  (6,"2022-06-14",8),
-                                                  (7,"2022-06-14",9),
-                                                  (8,"2022-06-14",7),
-                                                  (9,"2022-06-14",6),
-                                                  (10,"2022-06-14",10);
+INSERT INTO "order"(order_date,user_fk) VALUES ('2021-01-12',1),
+                                                  ('2021-07-07',3),
+                                                  ('2020-12-03',2),
+                                                  ('2022-06-14',5),
+                                                  ('2022-06-14',4),
+                                                  ('2022-06-14',8),
+                                                  ('2022-06-14',9),
+                                                  ('2022-06-14',7),
+                                                  ('2022-06-14',6),
+                                                  ('2022-06-14',8);
 
-INSERT INTO meal(id,name,description,portion_number,publication_date,user_fk,category_fk,order_fk,image)
- VALUES (1,"Pizza-Veggie","Plat frais à chauffer au four à une temperature ne depassant pas 200 degrés",2,"2021-01-01",1,2,1,null ),
-(2,"Carbonita-Do-Madrid","Plat chaud à manger le jour meme",1,"2020-12-01",2,5,3,null );
+INSERT INTO meal(name,description,portion_number,publication_date,user_fk,category_fk,order_fk,image)
+ VALUES ('Pizza-Veggie','Plat frais à chauffer au four à une temperature ne depassant pas 200 degrés',2,'2021-01-01',1,2,1,null ),
+('Carbonita-Do-Madrid','Plat chaud à manger le jour meme',1,'2020-12-01',2,5,3,null );
 
