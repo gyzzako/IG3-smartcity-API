74308480c34af3a85f270642c4c67e8ef8b0cda4
gyzzako <arnaud.berg1999@gmail.com>
2021-12-20 01:33:01 +0100

    back

--- src/app.js
+++ src/app.js
@@ -3,11 +3,10 @@ const cors = require('cors')
 const Router = require('./V1/routes');
 const express = require('express');
 const app = express();
-const port = process.env.PORT ?? 3001; //pour le port de heroku ou celui que nous choisissons pour localhost
-const path = require('path');
+const port = 3001;
 
 app.use(cors());
-app.use(express.static(path.join(__dirname,'./V1/upload')));
+app.use(express.static('./src/V1/upload'));
 app.use(express.json());
 app.use(Router);
 
